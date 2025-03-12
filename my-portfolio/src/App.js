26956c631bac940a7fc75e6d7053d7def59a4154
Ibrahim kocak <48657333+Ibo66139@users.noreply.github.com>
2025-03-12 01:08:10 +0100

    Image added
    Components created
    
    Display grid added
    
    CSS improvements

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 import Navbar from "./components/navbar.jsx";
+import Category from "./components/category.jsx";
+import Main from "./components/main.jsx";
+
 
 class App extends Component {
   state = {  } 
   render() { 
     return ( 
+      <div className="Gird-Container">
 
-      <Navbar/>
+            <Navbar/>
+            <Main/>
+            <Category/>
+      
+      </div>
+      
 
     );
   }
--- my-portfolio/src/components/navbar.jsx
+++ my-portfolio/src/components/navbar.jsx
@@ -6,13 +6,18 @@ class Navbar extends Component {
     render() {
         return (
 
-        <nav>
+        <nav className="Navbar">
             <a href="/home">Home</a>
-            <a href="/projects">Projects</a>
-            <a href="/about">About Me</a>
-            <a href="/contact">Contact</a>
+
+        <div className="nav-links">
+                <a href="/projects">Projects</a>
+                <a href="/about">About Me</a>
+                <a href="/contact">Contact</a>
+        </div>
+            
+            
         </nav>
-        
+
         );
     }
 }
